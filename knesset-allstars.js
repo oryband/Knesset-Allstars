c8fd0561e1aead826a364053101e4b30f5db7ef3
Ory Band <oryband@gmail.com>
2011-02-10 10:43:05 +0200

    Initial commit.

---
+++ knesset-allstars.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    /*var url = 'http://oknesset.org/api/member/801/';*/
+    /*var url = 'http://localhost:8000/api/member/771/';*/
+    var url = 'http://localhost:8000/api/member/';
+    var callback = function (data, textStatus, xhr) {
+        var template = '<ul> {{#member}} <li> <img src="{{img_url}}"> </li> {{/member}} </ul>';
+        var images = Mustache.to_html(template, {"member": data} );
+        $('#members').html(images);
+    };
+
+    $.getJSON(url, {}, callback);
+});
+
